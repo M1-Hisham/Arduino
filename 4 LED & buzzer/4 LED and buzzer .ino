7c0a8c32b4c8dd346297486d15b44326c5849ebc
Mohamed Hisham <128136684+M1-Hisham@users.noreply.github.com>
2023-08-25 19:19:17 +0300

    Add files via upload

---
+++ 4 LED & buzzer/4 LED and buzzer .ino
@@ -0,0 +1,54 @@
+int led[4]={8,9,10,11};
+#define time 1000
+#define buzzer 12
+void setup()
+{
+  pinMode(buzzer,OUTPUT);
+  pinMode(13,INPUT);  
+  for(int i=0;i==4;i++)
+  {
+    pinMode(led[i],OUTPUT);
+  }
+}
+void loop()
+{
+  if(digitalRead(13)>0)
+  {
+  for(int y=0;y<4;y++)
+  {
+    for(int j=0;j<4;j++)
+    {
+   
+      digitalWrite(led[j],LOW);
+    }
+    digitalWrite(led[y],HIGH);
+    delay(time);
+      if(led>0)
+  {
+    digitalWrite(buzzer,HIGH);
+    delay(100);
+    digitalWrite(buzzer,LOW);
+  }    
+  }
+
+  for(int y=4;y>=0;y--)
+  { 
+    for(int j=0;j<4;j++)
+    {
+      digitalWrite(led[j],LOW);
+    }
+    digitalWrite(led[y],HIGH);
+    delay(time);
+      if(led>0)
+  {
+    digitalWrite(buzzer,HIGH);
+    delay(50);
+    digitalWrite(buzzer,LOW);
+  }    
+  }
+  }
+  else{for(int c=0;c<4;c++)
+  {
+    digitalWrite(led[c],1);
+  }}
+}
